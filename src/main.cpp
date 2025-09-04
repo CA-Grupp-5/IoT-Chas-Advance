b69b9950020412d54f55419144c62fc0f8ab8605
Marco-Lind-ChasAcademy <marco.lind@chasacademy.se>
2025-09-05 00:25:03 +0200

    Simple WiFi server that receives data, then sends a message back to the client

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "secrets.h"
 
-// put function declarations here:
-int myFunction(int, int);
+WiFiClient client;
+WiFiServer server(5000);
+uint8_t buffer[4096];
+char message[64];
+size_t bytes_read;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+    Serial.begin(115200);
+
+    // ssid and password should be placed in secrets.h
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting server to Wifi");
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.print("Connected.\nServer IP: ");
+    Serial.println(WiFi.localIP());
+
+    server.begin();
+
+    Serial.println("Server is listening...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    client = server.available();
+    if (client)
+    {
+        Serial.println("Client connected.");
+        bytes_read = client.read(buffer, sizeof(buffer) - 1);
+        buffer[bytes_read] = '\0';
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+        snprintf(message, sizeof(message), "Server received %d bytes.", bytes_read);
+
+        client.print(message);
+        client.stop();
+        
+        Serial.println("Server is listening...");
+    }
+    else
+        delay(1000);
 }
