a70929b0f6b0d09d152219a595ad5aaed639ad16
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-09-22 23:38:01 +0200

    auto format files with clang format

--- Arduino/include/MockNetwork.h
+++ Arduino/include/MockNetwork.h
@@ -3,14 +3,17 @@
 #include "NetworkInterface.h"
 #include "WiFiMockType.h"
 
-class MockNetwork : public NetworkInterface {
-public:
+class MockNetwork : public NetworkInterface
+{
+  public:
     int current_status = MOCK_WL_IDLE_STATUS;
-    int begin(const char* ssid, const char *passphrase) override {
+    int begin(const char *ssid, const char *passphrase) override
+    {
         current_status = MOCK_WL_CONNECTED;
         return current_status;
     };
-    int status() const override {
+    int status() const override
+    {
         return current_status;
     }
 };
--- Arduino/include/NetworkInterface.h
+++ Arduino/include/NetworkInterface.h
@@ -1,25 +1,26 @@
 #ifndef NETWORK_INTERFACE_H
 #define NETWORK_INTERFACE_H
-#include <stdint.h>
 #include <cstddef>
+#include <stdint.h>
 
-class NetworkInterface {
-public:
-    virtual int begin(const char* ssid, const char *passphrase) = 0;
+class NetworkInterface
+{
+  public:
+    virtual int begin(const char *ssid, const char *passphrase) = 0;
     virtual int status() const = 0;
-    virtual ~NetworkInterface() {};
-
+    virtual ~NetworkInterface(){};
 };
 
-class ClientInterface {
-public:
+class ClientInterface
+{
+  public:
     virtual int connect(uint32_t ip, uint16_t port) = 0;
     virtual int available() = 0;
     virtual int read() = 0;
     // virtual int read(uint8_t *buf, size_t size) = 0;
-    virtual void stop() = 0;
+    virtual void    stop() = 0;
     virtual uint8_t connected() = 0;
-    virtual ~ClientInterface() {};
+    virtual ~ClientInterface(){};
 };
 
 #endif
