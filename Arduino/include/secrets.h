e8c4baa10dff7ea90be9cb0d139d0d71bfbdc81d
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-09-29 13:37:29 +0200

    add secrets and update github workflows

---
+++ Arduino/include/secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID "Mangoriet2gh"
+#define SECRET_PASSWORD "London12"
+
+#define SERVER_IP1 192
+#define SERVER_IP2 168
+#define SERVER_IP3 1
+#define SERVER_IP4 184
+#define SERVER_PORT 5000
---
+++ S3/include/secrets.h
@@ -0,0 +1,5 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+const char *ssid = "Mangoriet2gh";
+const char *password = "123";
+#endif
