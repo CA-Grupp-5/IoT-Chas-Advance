ac42227fd6530e23c38d2c1d576054d313921b67
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-10-30 14:12:31 +0100

    update placeholder values in example files

--- S3/include/secrets.example.h
+++ S3/include/secrets.example.h
@@ -1,5 +1,7 @@
 #ifndef SECRETS_EXAMPLE_H
 #define SECRETS_EXAMPLE_H
-#define SECRET_SSID "your_wifi_ssid"
-#define SECRET_PASSWORD "your_wifi_password"
+#define SECRET_SSID "your-wifi-ssid"
+#define SECRET_PASSWORD "your-wifi-password"
+#define AZURE_HOST "azure-host-address.net"
+#define AZURE_PORT 000
 #endif
