8b2d52db85afe2c86857f1a1157273507cf5c575
islandskan <89599893+islandskan@users.noreply.github.com>
2025-11-03 09:35:13 +0000

    CI pre-commit: Auto-formatting and linting

--- S3/src/main.cpp
+++ S3/src/main.cpp
@@ -4,23 +4,16 @@ WifiData   wifi_data(LOCAL_PORT, LOCAL_IP, SUBNET, GATEWAY, PRIMARY_DNS);
 SensorData sensor_data;
 size_t     bytes_read;
 
-
-
-
 void setup()
 {
     int i;
 
     Serial.begin(115200);
 
-    for (i = 0; i < 7; i++)
-        printStringDelay(1000, ".");
+    for (i = 0; i < 7; i++) printStringDelay(1000, ".");
 
     wifiInit(&wifi_data);
 }
-
-
-
 
 void loop()
 {
